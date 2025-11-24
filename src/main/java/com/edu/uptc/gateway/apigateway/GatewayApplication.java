380c28c6f30cdc8d76b1a297b0afc2a6cf173747
LeonardoAvila18 <145791768+mainAvilaMiguel@users.noreply.github.com>
2025-11-23 21:53:43 -0500

    Api gateway base

---
+++ src/main/java/com/edu/uptc/gateway/apigateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.gateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
